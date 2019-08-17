2c877947941be4a095a8e9d7b4ccd2b707373fef
Asmaathabet <asmathabet200@hotmail.com>
2019-08-17 21:26:01 +0300

    change completed value in the server

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,6 +3,11 @@ import { Observable } from 'rxjs';
 import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { Todo } from '../models/Todo';
 
+const httpOptions ={
+  headers : new HttpHeaders({
+   'Content-Type': 'application/json' 
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +18,14 @@ export class TodoService {
 
   constructor(private http:HttpClient) { }
 
+   // Get Todos 
   getTodos():Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosURL}${this.todosLimit}`);
+  }
+
+  // Toggel completed 
+  toggleCompleted(todo : Todo):Observable<any>{
+    const url = `${this.todosURL}/${todo.id}`;
+    return this.http.put(url ,todo , httpOptions)
   }
 }
